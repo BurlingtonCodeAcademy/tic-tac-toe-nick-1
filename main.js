52f3be4e61ddd363c57eaf52a3b95f98e66a1f1b
Dalton M <dnmorgan96@gmail.com>
2021-02-17 15:54:44 -0500

    merging work

---
+++ main.js
@@ -0,0 +1,10 @@
+let c0 = document.getElementById("cell-0")
+let c1 = document.getElementById("cell-1")
+let c2 = document.getElementById("cell-2")
+let c3 = document.getElementById("cell-3")
+let c4 = document.getElementById("cell-4")
+let c5 = document.getElementById("cell-5")
+let c6 = document.getElementById("cell-6")
+let c7 = document.getElementById("cell-7")
+let c8 = document.getElementById("cell-8")
+
