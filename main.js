ba6d3e3a1ddbd0d65666cf71dfa2bc5c87898ba2
NickJL524 <nicklamothe524@gmail.com>
2021-02-18 09:18:28 -0500

    tictac

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-let c0 = document.getElementById("cell-0")
-let c1 = document.getElementById("cell-1")
-let c2 = document.getElementById("cell-2")
-let c3 = document.getElementById("cell-3")
-let c4 = document.getElementById("cell-4")
-let c5 = document.getElementById("cell-5")
-let c6 = document.getElementById("cell-6")
-let c7 = document.getElementById("cell-7")
-let c8 = document.getElementById("cell-8")
+// let c0 = document.getElementById("cell-0");
+// let c1 = document.getElementById("cell-1");
+// let c2 = document.getElementById("cell-2");
+// let c3 = document.getElementById("cell-3");
+// let c4 = document.getElementById("cell-4");
+// let c5 = document.getElementById("cell-5");
+// let c6 = document.getElementById("cell-6");
+// let c7 = document.getElementById("cell-7");
+// let c8 = document.getElementById("cell-8");
 
+let cells = document.getElementsByClassName('cell')
+
+for(cell of cells)
+
+
+
+cells.addEventListener("click", draw);
+
+function draw(){
+    cell.textContent = "X"
+    
+}
